7f2cd2ad29bc7db3b2e40c06644220c2ac4d886f
zainjumuah <your.email@example.com~>
2026-01-11 22:12:12 +0100

    gql mutation

--- crm/settings.py
+++ crm/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 CRONJOBS = [
     ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+    ('0 */12 * * *', 'crm.cron.update_low_stock'),
 ]
 
 MIDDLEWARE = [
--- schema.py
+++ schema.py
@@ -1,15 +1,39 @@
 
 import graphene
+from crm.models import Product
 
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
 
 schema = graphene.Schema(query=Query)
 
-=======
-import graphene
+class ProductType(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    stock = graphene.Int()
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
 
-schema = graphene.Schema(query=Query)
+class UpdateLowStockProducts(graphene.Mutation):
+    class Arguments:
+        pass
+
+    success = graphene.String()
+    products = graphene.List(ProductType)
+
+    def mutate(self, info):
+        low_stock_products = Product.objects.filter(stock__lt=10)
+        updated_products = []
+
+        for product in low_stock_products:
+            product.stock += 10
+            product.save()
+            updated_products.append(product)
+
+        return UpdateLowStockProducts(
+            success="Low stock products updated successfully",
+            products=updated_products
+        )
+
+
+class Mutation(graphene.ObjectType):
+    update_low_stock_products = UpdateLowStockProducts.Field()
